9a87963c50e5df1331072e747454f502c4a72c17
afwill45 <afwill45@bu.edu>
2025-04-04 23:57:40 -0400

    updated configs for image domain acceptance

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: ['uploads.mangadex.org'],
+    },
+  };
+  
+  export default nextConfig;
+  
